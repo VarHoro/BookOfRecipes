f19ea88f9442d9d677a193a3d04803edabe4a402
VarHoro <varhoro@mail.ru>
2019-06-04 20:21:19 +0700

    rad_onclu

--- app/src/main/java/com/example/bookofrecipes/RecipeAdapter.java
+++ app/src/main/java/com/example/bookofrecipes/RecipeAdapter.java
@@ -2,6 +2,7 @@ package com.example.bookofrecipes;
 
 import android.content.Context;
 import android.content.Intent;
+import android.speech.RecognitionListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,26 +30,25 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
     public RecipeHolder onCreateViewHolder(@NonNull final ViewGroup parent, int viewType) {
         View itemView =  LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.recipe_item_layout, parent, false);
-        itemView.setOnClickListener(new View.OnClickListener() {
+        return new RecipeHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeHolder holder, final int position) {
+        Recipe current = recipes.get(position);
+        Picasso.get().load(current.getImages().get(0)).into(holder.imageView);
+        holder.titleTextView.setText(current.getName());
+        holder.descriptionTextView.setText(current.getDescription());
+        holder.card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int itemPosition = parent.indexOfChild(view);
-                Recipe item = recipes.get(itemPosition);
+                Recipe item = recipes.get(position);
                 Context context = view.getContext();
                 Intent intent = new Intent(context, RecipeActivity.class);
                 intent.putExtra("uuid", item.getUuid());
                 context.startActivity(intent);
             }
         });
-        return new RecipeHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecipeHolder holder, int position) {
-        Recipe current = recipes.get(position);
-        Picasso.get().load(current.getImages().get(0)).into(holder.imageView);
-        holder.titleTextView.setText(current.getName());
-        holder.descriptionTextView.setText(current.getDescription());
     }
 
     @Override
@@ -59,12 +59,14 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
     class RecipeHolder extends RecyclerView.ViewHolder {
         private TextView titleTextView, descriptionTextView;
         private ImageView imageView;
+        private View card;
 
         public RecipeHolder(@NonNull View itemView) {
             super(itemView);
             titleTextView = itemView.findViewById(R.id.recipe_title_text_view);
             descriptionTextView = itemView.findViewById(R.id.recipe_description_text_view);
             imageView = itemView.findViewById(R.id.recipe_image_view);
+            card = itemView.findViewById(R.id.recipe_card);
         }
     }
 }
