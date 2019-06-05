dac63747dc2771bc7ea47ec45c22fa67a8b09bc2
VarHoro <varhoro@mail.ru>
2019-06-05 17:13:57 +0700

    ia_fs_w

--- app/src/main/java/com/example/bookofrecipes/ImageActivity.java
+++ app/src/main/java/com/example/bookofrecipes/ImageActivity.java
@@ -1,16 +1,41 @@
 package com.example.bookofrecipes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class ImageActivity extends AppCompatActivity {
+
+    public static final int PERMISSION_REQUEST_READ_WRITE = 3;
     private ImageView imageView;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +49,59 @@ public class ImageActivity extends AppCompatActivity {
             Picasso.get().load(picName).into(imageView);
         } else{
             Toast.makeText(this, "Not found", Toast.LENGTH_SHORT).show();
+        }
+        fab = findViewById(R.id.fab_save);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(ImageActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ImageActivity.this,
+                            new String[]{
+                                    READ_EXTERNAL_STORAGE,
+                                    WRITE_EXTERNAL_STORAGE
+                            }, PERMISSION_REQUEST_READ_WRITE);
+                } else {
+                    saveImageToGallery();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_REQUEST_READ_WRITE:
+            {
+                if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    saveImageToGallery();
+                }
+                return;
+            }
+        }
+    }
+
+    void saveImageToGallery(){
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        File sdCard = Environment.getExternalStorageDirectory();
+        File dir = new File(sdCard.getAbsolutePath() + "/pictures");
+        dir.mkdir();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fileName = "Image-" + n + ".jpg";
+        File outFile = new File(dir, fileName);
+        try {
+            FileOutputStream outputStream = new FileOutputStream(outFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            Toast.makeText(this, "Saved to: " + outFile.getPath(), Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
